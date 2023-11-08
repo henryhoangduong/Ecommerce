c2031c04ee60557e5f953784b2f4851bb79199ff
DuongHuyHoang <henryhoangduong@gmail.com>
2023-11-08 10:50:25 +0700

    create order controllers

---
+++ backend/app/Http/Controllers/OrderController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Models\Orders;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function index(){}
+    public function store(Request $request)
+    {
+    }
+    public function show($id){}
+    public function edit($id){}
+    public function update(Request $request, $id){}
+}
