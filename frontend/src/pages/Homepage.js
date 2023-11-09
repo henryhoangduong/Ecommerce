b7175875dd2fbcf2f1b1262ee4d8f89782faf5e8
DuongHuyHoang <henryhoangduong@gmail.com>
2023-11-09 15:18:17 +0700

    update frontend

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -6,27 +6,22 @@ import { Outlet } from "react-router-dom";
 import axios from "axios";
 import './Homepage.css'
 import Pagination from "../components/Pagination";
+import Footer from "../components/Footer";
 
 function Homepage() {
-  
   return (
     <>
       <div
         class="bg-image">
-        
       </div>
       <div class="container">
         <div class="col">
           <Header class="row"></Header>
-          <span>.</span>
-          <br />
-          <span>.</span>
-          <br />
-          <span>.</span>
           <Productcard class="row align-center"></Productcard>
         </div>
         <Pagination></Pagination>
       </div>
+      <Footer></Footer>
     </>
   );
 }
