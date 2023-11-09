23cc102bd3d3e092f63a85dc5b5120801f6ff6e8
DuongHuyHoang <henryhoangduong@gmail.com>
2023-11-09 13:07:54 +0700

    add banner to frontend

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -4,22 +4,30 @@ import Producttable from "../components/Producttable";
 import Productcard from "../components/Productcard";
 import { Outlet } from "react-router-dom";
 import axios from "axios";
-
+import './Homepage.css'
+import Pagination from "../components/Pagination";
 
 function Homepage() {
   
   return (
-    <div class="container">
-      <div class="col">
-        <Header class="row"></Header>
-        <span>.</span>
-        <br />
-        <span>.</span>
-        <br />
-        <span>.</span>
-        <Productcard class="row align-center"></Productcard>
+    <>
+      <div
+        class="bg-image">
+        
       </div>
-    </div>
+      <div class="container">
+        <div class="col">
+          <Header class="row"></Header>
+          <span>.</span>
+          <br />
+          <span>.</span>
+          <br />
+          <span>.</span>
+          <Productcard class="row align-center"></Productcard>
+        </div>
+        <Pagination></Pagination>
+      </div>
+    </>
   );
 }
 
